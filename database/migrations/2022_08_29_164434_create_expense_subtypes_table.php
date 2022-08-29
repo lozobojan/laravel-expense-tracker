ce5634a6063921b723d9d746754c6aae40152e46
Bojan Lozo <bojanlo@maestralsolutions.com>
2022-08-29 18:48:09 +0200

    Added model ExpenseSubtype and corresponding classes

---
+++ database/migrations/2022_08_29_164434_create_expense_subtypes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\ExpenseType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expense_subtypes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('expense_type_id')->constrained('expense_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expense_subtypes');
+    }
+};
